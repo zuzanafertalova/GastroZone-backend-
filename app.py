ef22d84906f18e3c7fb130c1499ac150618f3bec
zuzana_fertalova <xfertalova@stuba.sk>
2022-03-28 16:14:57 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from flask import Flask, json
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
